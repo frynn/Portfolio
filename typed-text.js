9bacf2ff30cd18ddc36ac5aad1bfa16c92bacb67
frynn <grodnenec04@mail.ru>
2023-04-13 09:03:56 +0300

    main page implement

---
+++ typed-text.js
@@ -0,0 +1,9 @@
+var typing = new Typed(".typed", {
+    strings: [
+        "I'm front-end devloper based in Grodno, Belarus. Currently learning: Angular.",
+        "Hopefully you will be interested in",
+    ],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true, 
+});
